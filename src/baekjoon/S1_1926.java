c6e5a630b77f83ab60ce2f2b28521939b55b7ec7
sungmin <lsm1511@naver.com>
2025-05-11 23:11:00 +0900

    dfs,2차원 배열 카운트

---
+++ src/baekjoon/S1_1926.java
@@ -0,0 +1,76 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class S1_1926 {
+    static int[][] paper;
+    static boolean[][] visited;
+    static int n, m;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        paper = new int[n][m];
+        visited = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                paper[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int count = 0;
+        int maxArea = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (paper[i][j] == 1 && !visited[i][j]) {
+                    int area = bfs(i, j);
+                    count++;
+                    maxArea = Math.max(maxArea, area);
+                }
+            }
+        }
+
+        System.out.println(count);
+        System.out.println(maxArea);
+    }
+
+    static int bfs(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{x, y});
+        visited[x][y] = true;
+        int area = 1;
+
+        while (!queue.isEmpty()) {
+            int[] pos = queue.poll();
+            int cx = pos[0];
+            int cy = pos[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
+                    if (paper[nx][ny] == 1 && !visited[nx][ny]) {
+                        queue.add(new int[]{nx, ny});
+                        visited[nx][ny] = true;
+                        area++;
+                    }
+                }
+            }
+        }
+
+        return area;
+    }
+}
