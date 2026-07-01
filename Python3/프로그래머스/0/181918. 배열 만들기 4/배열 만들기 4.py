110caf41240658dfdb005e531f52c538935b5348
sungmin <68140696+lsm15111@users.noreply.github.com>
2026-07-01 10:18:05 +0900

    [level 0] Title: 배열 만들기 4, Time: 17.57 ms, Memory: 15 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
@@ -0,0 +1,14 @@
+def solution(arr):
+    i=0
+    stk = []
+    print(len(stk))
+    while i < len(arr):
+        if len(stk) == 0:
+            stk.append(arr[i])
+            i+=1
+        elif stk[len(stk)-1] <arr[i]:
+            stk.append(arr[i])
+            i+=1
+        else:
+            stk.pop()
+    return stk
