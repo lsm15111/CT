edfbc728830356fe7edecfc101dc06a33c2f8073
sungmin <68140696+lsm15111@users.noreply.github.com>
2026-07-08 20:47:19 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.00 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.py
@@ -0,0 +1,10 @@
+def solution(dot):
+    answer = 2
+    if dot[0] > 0:
+        if dot[1] > 0:
+            return 1
+        return 4
+    else:
+        if dot[1] > 0:
+            return 2
+        return 3
