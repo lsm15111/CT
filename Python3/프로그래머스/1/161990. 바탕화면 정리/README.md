6e500ec870ca2c114d17d20294a3bfb4dacbc1aa
sungmin <68140696+lsm15111@users.noreply.github.com>
2026-07-08 14:59:30 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.11 ms, Memory: 11.3 MB -BaekjoonHub

--- Python3/프로그래머스/1/161990. 바탕화면 정리/README.md
+++ Python3/프로그래머스/1/161990. 바탕화면 정리/README.md
@@ -1,10 +1,10 @@
 # [level 1] 바탕화면 정리 - 161990 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161990?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161990) 
 
 ### 성능 요약
 
-메모리: 11.5 MB, 시간: 0.04 ms
+메모리: 11.3 MB, 시간: 0.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 07월 08일 14:25:15
+2026년 07월 08일 14:59:24
 
 ### 문제 설명
 
--- Python3/프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ Python3/프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -1,26 +1,8 @@
 def solution(wallpaper):
-    
-    start_x = -1
-    start_y = -1
-    end_x = -1
-    end_y = -1
-
-    for wp_i,wp in enumerate(wallpaper):
-        if wp.find("#") !=-1: # #있을때
-            if start_y == -1: # start Y
-                start_y = wp_i
-            if end_y < wp_i: # end Y
-                end_y = wp_i
-            l = wp.find("#") # 좌 인덱스
-            r = wp.rfind("#") # 우 인덱스
-            print(l,r)
-            if start_x == -1: # start X
-                start_x = l
-            else:
-                start_x = min(start_x,l)
-            if end_x < r: # end X
-                end_x = r
-        
-    
-    answer = [start_y,start_x,end_y+1,end_x + 1]
-    return answer
+    x, y = [],[]
+    for i in range(len(wallpaper)):
+        for j in range(len(wallpaper[0])):
+            if wallpaper[i][j]== "#":
+                x.append(j)
+                y.append(i)
+    return [min(y),min(x),max(y)+1,max(x)+1]
